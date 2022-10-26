55a55a1e39632186b9ee3ba7e3c63dcd92523d1d
girma <gseminew@gmail.com>
2022-10-26 03:00:40 -0700

    Write a function that takes a pointer to an int as parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *
+ *  * reset_to_98 - Reset argument that points to the address to 98
+ *
+ *   * @n: pointer
+ *
+ *    * Return: void
+ *
+ *    */
+
+
+
+void reset_to_98(int *n)
+
+{
+
+
+
+	*n = 98;
+
+
+
+}
